1ec99d180f4733ede857f8c6d4ba6ee2c85647ef
Nick-Battjes_CaseysGH <nhbattjes@gmail.com>
2026-05-19 13:31:40 -0700

    fix typescript error

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,4 @@
 import { Footer } from "./components/Footer";
-import { Header } from "./components/Header";
 import { Hero } from "./components/Hero";
 import { Projects } from "./components/Projects";
 import "./App.css";
