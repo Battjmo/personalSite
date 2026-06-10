1d89d86b1540c6600de7814fe0efbfa7e95c40b5
Nick-Battjes_CaseysGH <nhbattjes@gmail.com>
2026-06-10 09:41:05 -0700

    feat: update color palette

--- src/App.tsx
+++ src/App.tsx
@@ -14,3 +14,13 @@ export default function App() {
     </>
   );
 }
+// // {"Scarlet Rush":"d64045","Frozen Water":"e9fff9","Frosted Blue":"9ed8db","Rich Cerulean":"467599","Twilight Indigo":"1d3354","Black":"000000"}
+
+/*
+scarlet: Primary, logo
+Frozen Water: background
+Frosted Blue: secondary
+Rich cerulean: headers
+Twilight: CTAs
+Black: text-primary
+*/
